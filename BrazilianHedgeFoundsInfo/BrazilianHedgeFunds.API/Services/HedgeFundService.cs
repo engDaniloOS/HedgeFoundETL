d0633afbd347e2a2206dd6806e76298b188fcf93
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2022-04-21 17:25:07 -0300

    Fixing default dates

--- BrazilianHedgeFoundsInfo/BrazilianHedgeFunds.API/Services/HedgeFundService.cs
+++ BrazilianHedgeFoundsInfo/BrazilianHedgeFunds.API/Services/HedgeFundService.cs
@@ -23,6 +23,7 @@ namespace BrazilianHedgeFunds.API.Services
         private const string INVALID_CNPJ = "Cnpj isn't valid |";
         private const string INVALID_START_DATE = "Start date isn't valid |";
         private const string INVALID_END_DATE = "End date isn't valid";
+        private readonly int OldestYearToFetchData;
         public HedgeFundService(ILogger<HedgeFundService> logger,
                                 IConfiguration configuration,
                                 IHedgeFundRecordRepository repository)
@@ -30,6 +31,7 @@ namespace BrazilianHedgeFunds.API.Services
             _logger = logger;
             _repository = repository;
             _configuration = configuration;
+            OldestYearToFetchData = int.Parse(_configuration["Params:OldesYearToProcess"]);
         }
 
         public async Task<HedgeFundOutDto> GetFundsBy(HedgeFundInDto inDto)
@@ -38,12 +40,18 @@ namespace BrazilianHedgeFunds.API.Services
             {
                 IsDataValid(inDto);
 
+                if (inDto.StartDate == DateTime.MinValue)
+                    inDto.StartDate = new DateTime(OldestYearToFetchData, 1, 1);
+
+                if (inDto.EndDate == DateTime.MinValue)
+                    inDto.EndDate = DateTime.Now;
+
                 var funds = await _repository.GetFundsBy(inDto);
 
                 if (funds.Count == 0)
                     return new HedgeFundOutDto { HedgeFundRecords = new List<HedgeFundRecord>(), IsInvalid = false };
 
-                var pageSize = (inDto.PageSize == 0) ? 
+                var pageSize = (inDto.PageSize == 0) ?
                     int.Parse(_configuration["Params:DefaulPageSize"]) : inDto.PageSize;
 
                 var pageIndex = (inDto.PageIndex == 0) ? 1 : inDto.PageIndex;
@@ -69,15 +77,13 @@ namespace BrazilianHedgeFunds.API.Services
         {
             var builder = new StringBuilder();
 
-            var oldestYearToFetchData = int.Parse(_configuration["Params:OldesYearToProcess"]);
-
             var isAnInvalidCnpj = !Regex.IsMatch(inDto.CNPJ, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)");
-            if (isAnInvalidCnpj) 
-               builder.Append(INVALID_CNPJ);
+            if (isAnInvalidCnpj)
+                builder.Append(INVALID_CNPJ);
 
             if (inDto?.StartDate != DateTime.MinValue)
             {
-                var isAnInvalidOldDate = inDto.StartDate.Year < oldestYearToFetchData;
+                var isAnInvalidOldDate = inDto.StartDate.Year < OldestYearToFetchData;
                 if (isAnInvalidOldDate)
                     builder.Append(INVALID_START_DATE);
             }
